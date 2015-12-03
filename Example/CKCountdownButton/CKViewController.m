7cea51f2564ca7f259cf0ffe5961823117fb9e44
Meiliang Dong <dongmeilianghy@sina.com>
2015-12-03 21:22:40 +0800

    Init commit.

---
+++ Example/CKCountdownButton/CKViewController.m
@@ -0,0 +1,36 @@
+//
+//  CKViewController.m
+//  CKCountdownButton
+//
+//  Created by Quanlong He on 08/02/2014.
+//  Copyright (c) 2014 Quanlong He. All rights reserved.
+//
+
+#import <CKCountdownButton/CKCountdownButton.h>
+
+#import "CKViewController.h"
+
+@interface CKViewController ()
+
+@property (weak, nonatomic) IBOutlet CKCountdownButton *fetchVerifyButton;
+
+@end
+
+@implementation CKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.fetchVerifyButton setTitle:@"(#)秒后重新获取" forState:UIControlStateDisabled];
+    self.fetchVerifyButton.backgroundColorForDisabledState = [UIColor lightGrayColor];
+    [self.fetchVerifyButton addTarget:self action:@selector(handleFetchVerifyCode:) forControlEvents:UIControlEventTouchUpInside];
+    self.fetchVerifyButton.count = 60;
+}
+
+- (void)handleFetchVerifyCode:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+@end
